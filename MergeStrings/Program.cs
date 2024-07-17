f6f4d8f9f47409c290f4dd966ee72fa2859a9604
ljpenn9 <laney.penn0538@gmail.com>
2024-07-17 18:55:30 -0500

    begin

---
+++ MergeStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public string? MergeAlternately(string word1, string word2)
+    {
+        return null;
+    }
+}
