0cd10a6c621cb26a73bb3110c3ffae252a77fc02
ljpenn9 <laney.penn0538@gmail.com>
2024-07-17 19:20:07 -0500

    solution accepted

--- MergeStrings/Program.cs
+++ MergeStrings/Program.cs
@@ -1,12 +1,33 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public static string MergeAlternately(string word1, string word2)
     {
-        Console.WriteLine("Hello, World!");
+        int shortWordLength = (word1.Length < word2.Length) ? word1.Length : word2.Length;
+        string merged = "";
+
+        for (int i = 0; i < shortWordLength; i++)
+        {
+            merged += word1[i];
+            merged += word2[i];
+        }
+
+        if (word1.Length > shortWordLength)
+            merged += word1.Substring(shortWordLength);
+        else
+            merged += word2.Substring(shortWordLength);
+
+        return merged;
     }
 
-    public string? MergeAlternately(string word1, string word2)
+    private static void Main(string[] args)
     {
-        return null;
+        string firstWord = "abcd";
+        string secondWord = "wxyz1234";
+        string merged = Program.MergeAlternately(firstWord, secondWord);
+        Console.WriteLine($"The first word is: {firstWord}");
+        Console.WriteLine($"The second word is: {secondWord}");
+        Console.WriteLine($"The merged together word is: {merged}");
     }
 }
